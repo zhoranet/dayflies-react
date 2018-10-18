a7e87f4a3b0a1a094eb4a783859d326b684bf828
George <job@zhora.net>
2018-10-17 23:11:28 -0700

    added ajax loaded events for selected date

--- src/containers/EventList/EventList.js
+++ src/containers/EventList/EventList.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import classes from './EventList.css';
 import { connect } from 'react-redux';
-import data from '../../data/sample-en.json';
 import Button from '../../components/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as actions from "../../store/actions";
 
 class EventList extends Component {
 
+    componentDidMount () {
+        var today = new Date();
+        this.props.onSelectDay(today.getFullYear(), today.getMonth(), today.getDate());
+    }
     
     render() {
-        const events = data.slice(0, 6)
-            .map((x, i) => <div key={'e' + i} ><h1>{x.name}</h1><p>{x.full}</p></div>);
+        const events = this.props.events
+            .map((x, i) => <div key={'e' + i} ><h4>{x.name}</h4><p>{x.short}</p></div>);
 
         return (
 
@@ -33,11 +36,13 @@ class EventList extends Component {
 const mapStateToProps = state => {
     return {
         date: state.selectedDate,
+        events: state.events
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {        
+        onSelectDay: (year, month, day) => dispatch(actions.selectDay(year, month, day)),
         onNextDay: (date) => dispatch(actions.nextDay(date)),
         onPrevDay: (date) => dispatch(actions.prevDay(date))
     };
--- src/store/actions/calendar.js
+++ src/store/actions/calendar.js
@@ -1,72 +1,83 @@
-import * as actionTypes from './actionTypes';
-import axios from '../../axios-calendar';
+import * as actionTypes from "./actionTypes";
+import axios from "../../axios-calendar";
 
-const fetchEventsSuccess = ( events ) => {
-    return {
-        type: actionTypes.FETCH_EVENTS_SUCCESS,
-        events: events
-    };
+const fetchEventsSuccess = events => {
+  return {
+    type: actionTypes.FETCH_EVENTS_SUCCESS,
+    events: events
+  };
 };
 
-const fetchEventsFail = ( error ) => {
-    return {
-        type: actionTypes.FETCH_EVENTS_FAIL,
-        error: error
-    };
+const fetchEventsFail = error => {
+  return {
+    type: actionTypes.FETCH_EVENTS_FAIL,
+    error: error
+  };
 };
 
 const fetchEventsStart = () => {
-    return {
-        type: actionTypes.FETCH_EVENTS_START
-    };
+  return {
+    type: actionTypes.FETCH_EVENTS_START
+  };
 };
 
 const incrementDay = (date, step) => {
-    var newDate = new Date(date);
-    newDate.setDate(newDate.getDate() + step);
-    return selectDate(newDate);
-}
+  var newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + step);
+  return selectDate(newDate);
+};
 
 const incrementMonth = (date, step) => {
-    var newDate = new Date(date);
-    newDate.setMonth(newDate.getMonth() + step);
-    return selectDate(newDate);
+  var newDate = new Date(date);
+  newDate.setMonth(newDate.getMonth() + step);
+  return selectDate(newDate);
+};
+
+const fetchAllEvents = () => {
+  return axios.get("/events/en.json").then(res => {
+    const fetchedEvents = [];
+    for (let key in res.data) {    
+      fetchedEvents.push({
+        ...res.data[key],
+        id: key
+      });
+    }
+    return fetchedEvents;
+  });
+};
+
+const filterByDate = (events, date) => {
+    let index = date.getDate() - 1;
+    console.log('index:' + index);
+    if(index > 15) index = index - 15;    
+    const randomSequence = '245913152879152614361725113313795819';
+    let pair = randomSequence.substring(index * 2, (index * 2) + 2);
+    return events.filter(x => parseInt(x.id, 10) >= parseInt(pair.charAt(0), 10) 
+        && parseInt(x.id, 10) <= parseInt(pair.charAt(1), 10));
 }
 
 const fetchEvents = date => {
-    return dispatch => {
-        dispatch(fetchEventsStart());
-        dispatch({
-            type: actionTypes.SELECT_DATE,
-            selectedDate: date
-        });
-        axios.get( '/events/en.json' )
-                .then( res => {
-                    const fetchedEvents = [];
-                    for ( let key in res.data ) {
-                        fetchedEvents.push( {
-                            ...res.data[key],
-                            id: key
-                        } );
-                    }
-                    dispatch(fetchEventsSuccess(fetchedEvents));
-                } )
-                .catch( err => {
-                    dispatch(fetchEventsFail(err));
-                } );
-    }
+  return dispatch => {
+    dispatch(fetchEventsStart());
+    dispatch({
+      type: actionTypes.SELECT_DATE,
+      selectedDate: date
+    });
+
+    fetchAllEvents()
+      .then(fetchedEvents => filterByDate(fetchedEvents, date))
+      .then(filteredEvents => dispatch(fetchEventsSuccess(filteredEvents)))
+      .catch(err => {
+        dispatch(fetchEventsFail(err));
+      });
+  };
 };
 
-const selectDate =(date) => {
+const selectDate = date => dispatch => dispatch(fetchEvents(date));
 
-    fetchEvents(date);
-
-    
-}
-
-export const selectDay = (year, month, day) => selectDate(new Date(year, month, day));
-export const prevDay = date =>  incrementDay(date, -1);
+export const selectDay = (year, month, day) => dispatch =>
+  dispatch(selectDate(new Date(year, month, day)));
+export const prevDay = date => incrementDay(date, -1);
 export const nextDay = date => incrementDay(date, 1);
 export const prevMonth = date => incrementMonth(date, -1);
 export const nextMonth = date => incrementMonth(date, 1);
-
