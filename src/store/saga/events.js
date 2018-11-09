f9a083f53d0420dcf6d856ff5b15c191edf48867
George <job@zhora.net>
2018-11-08 19:36:10 -0800

    fixed compilation error

--- src/store/saga/events.js
+++ src/store/saga/events.js
@@ -7,7 +7,7 @@ export function* fetchEventsPageSaga(action) {
 	yield put(actions.fetchEventsStart());
 	
 	try {
-		const res = yield axios.get(`/events/${action.language}.json`);
+		const res = yield axios.get(`/events/en.json?shallow=true`);
 		const fetchedEvents = [];
 		for (let key in res.data) {
 			fetchedEvents.push({
@@ -16,9 +16,8 @@ export function* fetchEventsPageSaga(action) {
 			});
 		}
 
-		const filteredEvents = filterByDate(fetchedEvents, action.date);
-
-		yield put(actions.fetchEventsSuccess(action.date, action.language, filteredEvents));
+		
+		yield put(actions.fetchEventsSuccess(action.date, action.language, fetchedEvents));
 	} catch (error) {
 		yield put(actions.fetchEventsFail(error));
 	}
