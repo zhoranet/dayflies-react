03081f8dfd21758c67e7a0482aacf3c47659e924
George <job@zhora.net>
2018-11-27 00:35:17 -0800

    added eventhandler for eventedit form

--- src/containers/EventEdit/EventEdit.js
+++ src/containers/EventEdit/EventEdit.js
@@ -7,23 +7,53 @@ import * as actions from "../../store/actions";
 import { withRouter } from "react-router-dom";
 
 export class EventEdit extends Component {
+	state = {
+		inputElements: {
+			date: { elemtType: "input", label: "Date" },
+			name: { elemtType: "input", label: "Name" },
+			desc: { elemtType: "input", label: "Description" }
+		}
+	};
 
-	inputElements = [
-		{ elemtType: 'input', label: 'Date' },
-		{ elemtType: 'input', label: 'Name' },
-		{ elemtType: 'input', label: 'Description' },
-	];
+	onSubmitHandler = event => {
+		event.preventDefault();
+
+		// SUBMIT
+
+		console.log("Submit");
+	};
+
+	inputChangedHandler = (event, inputIdentifier) => {
+		const updatedFormElement = {
+			...updatedOrderForm[inputIdentifier]
+		};
+		updatedFormElement.value = event.target.value;
+	};
 
 	render() {
+		const formElementsArray = [];
+		for (let key in this.inputElements) {
+			formElementsArray.push({
+				id: key,
+				config: this.inputElements[key]
+			});
+		}
 
-		var elements = this.inputElements.map(x => <Input {...x} ></Input>);
+		var elements = formElementsArray.map(x => (
+			<Input
+				key={x.id}
+				{...x.config}
+				value={x.config.value}
+				changed={event => this.inputChangedHandler(event, x.id)}
+			/>
+		));
 
-		return (			
-			<div className={classes.EventEdit}>
+		return (
+			<form className={classes.EventEdit} onSubmit={this.onSubmitHandler}>
 				<h2>Edit</h2>
 				{elements}
 				<Button>OK</Button>
-			</div>	
+			</form>
 		);
 	}
 }
